64613feb360e04af60c702a0e2b9ebdd5e890383
BT_1N3_02 <kkhambhla0604@bths.edu>
2019-04-01 08:33:13 -0400

    fbfg

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,20 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice {
+    private int first;
+    private int second;
+
+    public MultPractice(int num1, int num2)
+    {
+        first = num1;
+        second = num2;
+    }
+    public String getProblem() {
+        return first + " TIMES " + second;
+    }
+
+    public void nextProblem() {
+        second++;
+    }
 }
